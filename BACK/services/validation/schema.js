f8dbe04828071f7424e52cd128c75b621d91eb4c
FlaSh0211 <nexus2493@gmail.com>
2020-10-13 17:43:40 +0000

    added joi lib & uerSchema check logic

---
+++ BACK/services/validation/schema.js
@@ -0,0 +1,15 @@
+import joi from 'joi';
+
+const Joi = require('joi');
+
+export const userSchema = Joi.object({
+	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+	studentId: Joi.string().alphanum().min(3).max(30).required(),
+	name: Joi.string().required(),
+	confirmation_code: Joi.string().required(),
+	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+	repeat_password: Joi.ref('password'),
+	access_token: [Joi.string(), Joi.number()],
+})
+	.xor('password', 'access_token')
+	.with('password', 'repeat_password');
