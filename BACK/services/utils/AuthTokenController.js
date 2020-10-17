680b05570159e26248013024edfb3f255df2db85
김영환 <00ghks22@naver.com>
2020-10-17 15:21:15 +0000

    jwt 인증 토큰 구현

--- BACK/services/utils/AuthTokenController.js
+++ BACK/services/utils/AuthTokenController.js
@@ -3,20 +3,24 @@ const passport = require('passport');
 require('dotenv').config();
 
 exports.create = function (req, res) {
-    passport.authenticate('local', {session: false}, (err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                message: 'Something is not right',
-                user   : user
-            });
-        }
-        req.login(user, {session: false}, (err) => {
-            if (err) {
-                res.send(err);
-            }
-            // jwt.sign('token내용', 'JWT secretkey')
-            const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
-            return res.json({user, token});
-        });
-    })(req, res);
+	passport.authenticate('jwt', {session: false}, (err, user) => {
+		if(!user.email) {
+			passport.authenticate('local', {session: false}, (err, user) => {
+				console.log('autheticate local')
+				if (err || !user.email) {
+					return res.status(400).json({
+						message: 'Login Fail',
+						user   : user
+					});
+				}
+				else {
+					// jwt.sign('token내용', 'JWT secretkey')
+					const token = jwt.sign('nexus2493', process.env.JWT_SECRET);
+					console.log(token,'aaaaaaaaaaaaaaaaaaaaaaaaaaa')
+					window.localStorage.setItem('token', token);
+					return res.json({user, token});
+				}
+			})(req,res)
+		}
+	})(req,res)
 };
