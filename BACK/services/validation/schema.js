56771a01866dd50f0b69a2ae56867853d11535b5
FlaSh0211 <nexus2493@gmail.com>
2020-10-24 10:08:29 +0000

    fix(validation check) update email validation check

--- BACK/services/validation/schema.js
+++ BACK/services/validation/schema.js
@@ -4,7 +4,10 @@ const Joi = require('joi');
 const { EMAIL_DOMAIN } = process.env;
 
 export const userSchema = Joi.object({
-	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['kr', 'com'] } }).pattern(new RegExp(EMAIL_DOMAIN)).required(),
+	email: Joi.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ['kr'] } })
+		.pattern(new RegExp(EMAIL_DOMAIN+'+\.ac+\.kr$'))
+		.required(),
 	studentId: Joi.string().alphanum().min(3).max(30).required(),
 	name: Joi.string().required(),
 	confirmation_code: Joi.string(),
@@ -19,4 +22,4 @@ export const boardSchema = Joi.object({
 	title: Joi.string().alphanum().min(0).max(30).required(),
 	contents: Joi.string(),
 	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-})
+});
