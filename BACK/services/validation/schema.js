9a8e15b8552d17a7bd3fa68e41cdbe4c802ff750
FlaSh0211 <nexus2493@gmail.com>
2020-10-24 09:53:47 +0000

    fix(validation check) updated email validation checking function

--- BACK/services/validation/schema.js
+++ BACK/services/validation/schema.js
@@ -1,9 +1,10 @@
 import joi from 'joi';
 
 const Joi = require('joi');
+const { EMAIL_DOMAIN } = process.env;
 
 export const userSchema = Joi.object({
-	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['kr', 'com'] } }).pattern(new RegExp(EMAIL_DOMAIN)).required(),
 	studentId: Joi.string().alphanum().min(3).max(30).required(),
 	name: Joi.string().required(),
 	confirmation_code: Joi.string(),
@@ -12,3 +13,10 @@ export const userSchema = Joi.object({
 	access_token: [Joi.string(), Joi.number()],
 }).xor('password', 'access_token');
 // .with('password', 'repeat_password');
+
+export const boardSchema = Joi.object({
+	type: Joi.string().alphanum().min(3).max(10).required(),
+	title: Joi.string().alphanum().min(0).max(30).required(),
+	contents: Joi.string(),
+	email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+})
